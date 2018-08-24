11d608b95cba24f161904a289c68383345bf9165
Chris Huxtable <chris@huxtable.ca>
2018-08-24 02:13:49 -0400

    Substantial restructure
    Converts to standard formatting.

--- src/syslog.cr
+++ src/syslog.cr
@@ -14,156 +14,174 @@
 
 require "./syslog/*"
 
-
 module Syslog
+  @@prefix : String = PROGRAM_NAME
+  @@facility : Facility = Facility::User
+  @@options : Options = Options::None
+  @@mask : UInt16 = 0xff_u16
 
+  # Sets the string to be prefixed to log messages.
+  def self.prefix=(prefix : String) : Nil
+    @@prefix = prefix
+    LibC.openlog(prefix, @@options.value, @@facility.value)
+  end
 
-	# MARK: - Defaults
+  # Returns the string to be prefixed to log messages.
+  def self.prefix : String
+    @@prefix
+  end
 
-	@@facility : Facility = Facility::User
-	class_property facility : Facility
+  # Sets the loggers default facility.
+  def self.facility=(facility : Facility) : Nil
+    @@facility = facility
+    LibC.openlog(@@prefix, @@options.value, @@facility.value)
+  end
 
-	def self.mask=(mask : Int) : Nil
-		LibC.setlogmask(mask)
-	end
+  # Returns the loggers default facility.
+  def self.facility : Facility
+    return @@facility
+  end
 
+  # Sets the loggers options.
+  def self.options=(options : Options) : Nil
+    @@options = options
+    LibC.openlog(@@prefix, @@options.value, @@facility.value)
+  end
 
-	# MARK: - Generics
+  # Returns the loggers options.
+  def self.options : Options
+    return @@options
+  end
 
-	# Initializes the log file for special processing.
-	# The use of this method is optional.
-	#
-	# NOTE: This API may be replaced in a future version.
-	def self.open(prefix : String, facility : Facility = @@facility, *options : Option) : Nil
-		@@facility = facility
-		options = options.reduce(0) { |acc, elm| (acc|elm.value) }
-		LibC.openlog(prefix, options, facility)
+  # Sets the loggers mask.
+  def self.mask=(mask : Int) : Nil
+    raise "Invalid mask given #{mask.inspect}" if 0 > mask > 255
+    @@mask = mask.to_u16
+    LibC.setlogmask(mask)
+  end
 
-#		syslog_data = syslog_data_init()
-#		LibC.openlog_r(prefix, options, facility, pointerof(syslog_data))
-	end
+  # Returns the loggers mask.
+  def self.mask : UInt16
+    return @@mask
+  end
 
-	# :ditto:
-	def self.open(prefix : String, facility : Facility = @@default_facility) : Nil
-		LibC.openlog(prefix, 0, facility)
+  # Sets all the loggers options at once.
+  # This is more efficient when changing many settings.
+  def self.setup(prefix : String, facility : Facility, options : Options, mask : Int) : Nil
+    @@prefix = prefix
+    @@facility = facility
+    @@options = options
+    LibC.openlog(@@prefix, @@options.value, @@facility.value)
 
-#		syslog_data = syslog_data_init()
-#		LibC.openlog_r(prefix, 0, facility, pointerof(syslog_data))
-	end
+    self.mask = mask
+  end
 
-	# Writes a message with a priority and facility.
-	def self.log(priority : Priority, facility : Facility, message : String) : Nil
-		LibC.syslog(priority.value|facility.value, message)
+  # Resets all the loggers options to their defaults.
+  def self.reset_defaults : Nil
+    setup(PROGRAM_NAME, Facility::User, Options::None, 0xff)
+  end
 
-#		syslog_data = syslog_data_init()
-#		LibC.syslog_r(priority.value|facility.value, pointerof(syslog_data), message)
-	end
+  # Closes the current logger.
+  # Useful for shard libraries where the prefix may be unloaded.
+  def self.close : Nil
+    LibC.closelog
+  end
 
-	# Closes the descriptor being used to write to the system logger, clearing special options.
-	# The use of this method is optional.
-	#
-	# NOTE: This API may be replaced in a future version.
-	def self.close() : Nil
-		@@facility = Facility::User
-		LibC.closelog()
+  # Writes a message with a priority and facility.
+  def self.log(priority : Priority, message : String, facility : Facility? = nil) : Nil
+    if facility
+      LibC.syslog(priority.value | facility.value, message)
+    else
+      LibC.syslog(priority.value, message)
+    end
+  end
 
-#		syslog_data = syslog_data_init()
-#		LibC.closelog_r(pointerof(syslog_data))
-	end
+  # Writes an emergency *syslog* message with an optional facility.
+  def self.emergency(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Emergency, message, facility)
+  end
 
+  # Writes an alert *syslog* message with an optional facility.
+  def self.alert(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Alert, message, facility)
+  end
 
-	# MARK: - Log By Level
+  # Writes an critical *syslog* message with an optional facility.
+  def self.critical(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Critical, message, facility)
+  end
 
-	# Writes an emergency *syslog* message with an optional facility.
-	def self.emergency(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Emergency, facility, message)
-	end
+  # Writes an error *syslog* message with an optional facility.
+  def self.error(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Error, message, facility)
+  end
 
-	# Writes an alert *syslog* message with an optional facility.
-	def self.alert(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Alert, facility, message)
-	end
+  # Writes an warning *syslog* message with an optional facility.
+  def self.warning(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Warning, message, facility)
+  end
 
-	# Writes an critical *syslog* message with an optional facility.
-	def self.critical(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Critical, facility, message)
-	end
+  # Writes an notice *syslog* message with an optional facility.
+  def self.notice(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Notice, message, facility)
+  end
 
-	# Writes an error *syslog* message with an optional facility.
-	def self.error(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Error, facility, message)
-	end
+  # Writes an info *syslog* message with an optional facility.
+  def self.info(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Info, message, facility)
+  end
 
-	# Writes an warning *syslog* message with an optional facility.
-	def self.warning(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Warning, facility, message)
-	end
+  # Writes an debug *syslog* message with an optional facility.
+  def self.debug(message : String, facility : Facility? = nil) : Nil
+    log(Priority::Debug, message, facility)
+  end
 
-	# Writes an notice *syslog* message with an optional facility.
-	def self.notice(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Notice, facility, message)
-	end
+  enum Facility
+    Authorization = LibC::LOG_AUTH
+    Privilage     = LibC::LOG_AUTHPRIV
+    Cron          = LibC::LOG_CRON
+    Daemon        = LibC::LOG_DAEMON
+    FTP           = LibC::LOG_FTP
+    Kernal        = LibC::LOG_KERN
+    LPR           = LibC::LOG_LPR
+    Mail          = LibC::LOG_MAIL
+    News          = LibC::LOG_NEWS
+    Syslog        = LibC::LOG_SYSLOG
+    User          = LibC::LOG_USER
+    UUCP          = LibC::LOG_UUCP
+    Local0        = LibC::LOG_LOCAL0
+    Local1        = LibC::LOG_LOCAL1
+    Local2        = LibC::LOG_LOCAL2
+    Local3        = LibC::LOG_LOCAL3
+    Local4        = LibC::LOG_LOCAL4
+    Local5        = LibC::LOG_LOCAL5
+    Local6        = LibC::LOG_LOCAL6
+    Local7        = LibC::LOG_LOCAL7
+  end
 
-	# Writes an info *syslog* message with an optional facility.
-	def self.info(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Info, facility, message)
-	end
+  enum Priority
+    Emergency = LibC::LOG_EMERG
+    Alert     = LibC::LOG_ALERT
+    Critical  = LibC::LOG_CRIT
+    Error     = LibC::LOG_ERR
+    Warning   = LibC::LOG_WARNING
+    Notice    = LibC::LOG_NOTICE
+    Info      = LibC::LOG_INFO
+    Debug     = LibC::LOG_DEBUG
+  end
 
-	# Writes an debug *syslog* message with an optional facility.
-	def self.debug(message : String, facility : Facility = @@facility) : Nil
-		log(Priority::Debug, facility, message)
-	end
+  @[Flags]
+  enum Options
+    Console    = LibC::LOG_CONS
+    NoDelay    = LibC::LOG_NDELAY
+    Delay      = LibC::LOG_ODELAY
+    PrintError = LibC::LOG_PERROR
+    PID        = LibC::LOG_PID
+  end
 
-
-	# MARK: - Enums
-
-	enum Facility
-		Authorization	= LibC::LOG_AUTH
-		Privilage		= LibC::LOG_AUTHPRIV
-		Cron			= LibC::LOG_CRON
-		Daemon			= LibC::LOG_DAEMON
-		FTP				= LibC::LOG_FTP
-		Kernal			= LibC::LOG_KERN
-		LPR				= LibC::LOG_LPR
-		Mail			= LibC::LOG_MAIL
-		News			= LibC::LOG_NEWS
-		Syslog			= LibC::LOG_SYSLOG
-		User			= LibC::LOG_USER
-		UUCP			= LibC::LOG_UUCP
-		Local0			= LibC::LOG_LOCAL0
-		Local1			= LibC::LOG_LOCAL1
-		Local2			= LibC::LOG_LOCAL2
-		Local3			= LibC::LOG_LOCAL3
-		Local4			= LibC::LOG_LOCAL4
-		Local5			= LibC::LOG_LOCAL5
-		Local6			= LibC::LOG_LOCAL6
-		Local7			= LibC::LOG_LOCAL7
-	end
-
-	enum Priority
-		Emergency	= LibC::LOG_EMERG
-		Alert		= LibC::LOG_ALERT
-		Critical	= LibC::LOG_CRIT
-		Error		= LibC::LOG_ERR
-		Warning		= LibC::LOG_WARNING
-		Notice		= LibC::LOG_NOTICE
-		Info		= LibC::LOG_INFO
-		Debug		= LibC::LOG_DEBUG
-	end
-
-	enum Options
-		Console		= LibC::LOG_CONS
-		NoDelay		= LibC::LOG_NDELAY
-		Delay		= LibC::LOG_ODELAY
-		PrintError	= LibC::LOG_PERROR
-		PID			= LibC::LOG_PID
-	end
-
-
-	# MARK: - Utilities
-
-#	macro syslog_data_init()
-#		LibC::SyslogData.new(log_stat: 0, log_tag: "", log_fac: LibC::LOG_USER, log_mask: 0xff)
-#	end
-
+  # {% if flag?(:openbsd) || flag?(:freebsd) %}
+  #   macro syslog_data_init
+  #     LibC::SyslogData.new(log_stat: 0, log_tag: "", log_fac: LibC::LOG_USER, log_mask: 0xff)
+  #   end
+  # {% end %}
 end
